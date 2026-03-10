89b8eb5fb9e24fac72892f45c922b756f70ea4f4
nienie0226-dot <nienie0226@gmail.com>
2026-03-10 11:56:40 +0800

    week3.py

---
+++ text1.py
@@ -0,0 +1,27 @@
+x = int(input("請輸入一個正整數："))
+
+if (x<=0):
+
+	print("您輸入的值小於等於0")
+
+else:
+
+	for i in range(x):
+
+		print(i)
+
+def square(y):
+
+	print("{} 的平方為 {}".format(y, y*y))
+
+x = int(input("請輸入一個正整數："))
+
+if (x<=0):
+
+	print("您輸入的值小於等於0")
+
+else:
+
+	for i in range(x):
+
+		square(i)
