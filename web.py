67defa06757748a4f935d8c353d55eaf554293b9
nienie0226-dot <nienie0226@gmail.com>
2026-03-17 12:11:22 +0800

    0317

---
+++ web.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	link="<h1>歡迎進入巴嚕的網站首頁2</h1>"
+	link+="<a href=/mis>課程</a><hr>"
+	link+="<a href=/today>今天日期</a><hr>"
+	link+="<a href=/about>關於巴嚕</a><hr>"
+	return link
+
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    year = str(now.year)
+    month =str(now.month)
+    day = str(now.day)
+    now = year +"年"+month+"月"+day+"日"
+    return render_template("today.html", datetime = str(now))
+@app.route("/about")
+def about():
+	return render_template("MIS2A.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
